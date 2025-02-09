5eb25f7c860421fa8bd80789848b448b94e58637
Neeraj-Gulavani <neerajgulavani@gmail.com>
2025-02-09 13:42:20 +0530

    added calorie api

---
+++ trackcal/mainpage/calorieapi.py
@@ -0,0 +1,22 @@
+import requests
+import os
+API_KEY = os.getenv("CN_API_KEY")
+def GetNutrition(query):
+
+    url = "https://api.calorieninjas.com/v1/nutrition?query="
+    r = requests.get(url+query, headers={'X-Api-Key': API_KEY})
+    if r.status_code == requests.codes.ok:
+        return r.text
+    else:
+        return "Error:" + str(r.status_code) + r.text
+
+
+def GetRecipe(query):
+    url="https://api.calorieninjas.com/v1/recipe?query="
+    r=requests.get(url+query, headers={'X-Api-Key:': API_KEY})
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print("Error:", r.status_code, r.text)
+if __name__=='__main__':
+    print(GetNutrition('burger'))
