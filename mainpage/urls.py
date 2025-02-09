7dd4fe0ad6ebe5dbc48e9d0432d134648fd7e62f
gandharvajoshi <gandharvajoshi2006@gmail.com>
2025-02-09 16:49:57 +0530

    linked two apps content and mainpage

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-    path("",views.index,name="mainpage")
+    path("",views.index,name="mainpage"),
+    path("ready/",views.redirect,name="redirect")
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 def index(request):
     return render(request,"mainpage/main.html",{})
 
+def redirect(request):
+    if request.method=='POST':
+        redirect("")
+from django.shortcuts import redirect
+
+
 # Create your views here.
